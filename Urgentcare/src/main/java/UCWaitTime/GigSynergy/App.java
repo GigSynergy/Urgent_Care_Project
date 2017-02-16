b963a764521a0d5f8d1567a82a69d8f0c7a00160
Tannaz Rezaei <tannazrezaei@yahoo.com>
2017-02-16 14:08:12 -0800

    new changes are commited.

--- Urgentcare/src/main/java/UCWaitTime/GigSynergy/App.java
+++ Urgentcare/src/main/java/UCWaitTime/GigSynergy/App.java
@@ -1,11 +1,11 @@
-package util;
+package UCWaitTime.GigSynergy;
 import java.sql.*;
-public class App {
+public class App{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*try{
+		try{
 			// 1. Get a Connection to DB
 			
 			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/urgentcare","user","password");
@@ -14,19 +14,19 @@ public class App {
 			// 3. Create a ResultSet
 			
 			myStat.executeUpdate("INSERT INTO tblpatient(`Patient_ID`, `Patient_Phone`, `Patient_F_Name`, `Patient_M_Name`, `Patient_L_Name`, `Service_Date`, `Service_Time`, `Facility_ID`) "
-					+ "VALUES (10, 12345678, 'Shubhangi', 'V', 'Shimpi', 2017-02-02, 12:15:20, 1)");
+					+ "VALUES (10000, 12345678, 'Shubhangi', 'V', 'Shimpi', 2017-02-02, 12:15:20, 1)");
 			
 			ResultSet myRs = myStat.executeQuery("select * from tblpatient");
 			
 			
 			// Try to print the result 
-			/*while (myRs.next()){
+			while (myRs.next()){
 				System.out.println(myRs.getString("Patient_F_Name"));
 			}
 		}
-		catch (Exception exception){
+		catch (Exception exc){
 					exc.printStackTrace();
-		}*/
+		}
 	}
 
 }
