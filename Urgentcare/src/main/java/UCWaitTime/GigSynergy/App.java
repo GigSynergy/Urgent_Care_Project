56d5dd8a468040f3ddf35a84ca40a58c1d523d2b
Tannaz Rezaei <tannazrezaei@yahoo.com>
2017-02-24 22:55:09 -0800

    Database current date and time issue is resolved. Both Tables are altered.

--- Urgentcare/src/main/java/UCWaitTime/GigSynergy/App.java
+++ Urgentcare/src/main/java/UCWaitTime/GigSynergy/App.java
@@ -1,10 +1,13 @@
 package UCWaitTime.GigSynergy;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 public class App{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		    // 20150928_161823
 		try{
 			// 1. Get a Connection to DB
 			
@@ -14,11 +17,25 @@ public class App{
 			// 3. Create a ResultSet
 			
 			System.out.println(myConn);
+			//myStat.executeUpdate("INSERT INTO tblfacility VALUES (1,`Woodbridge`,123456, `Irvine`,92612 , 10 , 4 )");
+		//myStat.executeUpdate("INSERT INTO tblfacility(`Facility_ID`,`Facility_Name`,`Facility_Phone`,`Facility_Address`,`Facility_ZipCode`, `Facility_QN`, `Facility_Rating`)"
+			//	+ "VALUES (3, 'Woodbridge',123456,'Irvine',92612, 10,4 )");
 			
-		//myStat.executeUpdate("INSERT INTO tblfacility(`Facility_ID`, `Facility_Phone`, `Facility_Address`, `Facility_Rating`)"
-		//			+ "VALUES (2, 10101010,'Pomona' , 3)");
-		//myStat.executeUpdate("INSERT INTO tblpatient(`Patient_ID`, `Patient_Phone`, `Patient_F_Name`, `Patient_M_Name`, `Patient_L_Name`, `Service_Date`, `Service_Time`, `Facility_ID`)" 
-			//		+ "VALUES (1, 12345678, 'Tannaz', '', 'Rezaei', '0000-00-00', '00:00:00', 1)");	
+		/*	String CurrTime =( new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime()) );
+			String CurrDate = (new SimpleDateFormat("MM-dd-yyyy").format(Calendar.getInstance().getTime()));
+			System.out.println(CurrTime);
+			System.out.println(CurrDate);
+			*/
+			
+			java.sql.Date Date = new java.sql.Date(new java.util.Date().getTime());
+			System.out.println(Date);
+			java.sql.Time Time = new java.sql.Time(new java.util.Date().getTime());
+			System.out.println(Time);
+			
+			
+		myStat.executeUpdate("INSERT INTO tblpatient(`Patient_ID`, `Patient_Phone`, `Patient_F_Name`, "
+			+ "`Patient_M_Name`, `Patient_L_Name`, `Service_Date`, `Service_Time`, `Facility_ID`)" 
+				+ "VALUES (1, 12345678, 'Tannaz', '', 'Rezaei','"+Date+"'  , '"+Time+"' , 1)");	
 		ResultSet myRs = myStat.executeQuery("select * from tblfacility");
 			
 			
